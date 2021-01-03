3c6f264902916b33b64992e4ff1b2fee7ccc047a
Aaron Cimolini <aaron@cimolini.com>
2021-01-03 09:52:39 -0800

    Adds Strapi CMS to the project.

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c31d976a944e1855e5ad19aabb81afa5'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4fac19e3-2877-497f-83de-03e9f838f8a5'
+};
